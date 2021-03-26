9c2e15a3f7569afd107b248b40d4d82842414dc2
SUMSCOPE\wengang.chen <yichenw7@126.com>
2021-03-26 16:57:23 +0800

    add route update

--- antd-demo/src/App.js
+++ antd-demo/src/App.js
@@ -1,25 +1,27 @@
+import React, {Component, PureComponent} from 'react'
+import { BrowserRouter as Router,Route ,Link} from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
+import {render} from "@testing-library/react";
+import List from './List'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+        <div className="App">
+            <Router>
+                <div>
+                    <h1>App</h1>
+                    <ul>
+                        <li><Link to="/list">List</Link></li>
+                    </ul>
+                    <hr/>
+                    <Route path="/list" component={List} />
+                </div>
+            </Router>
+        </div>
+    );
+  }
 }
 
 export default App;
---
+++ antd-demo/src/List.js
@@ -0,0 +1,11 @@
+import React,{Component} from 'react'
+
+class List extends Component{
+    render(){
+        return (
+            <h1>this is List</h1>
+        )
+    }
+}
+
+export default List;
